e297f3e14dece94496d4a60de15289ff44b7aa37
George <george_roots@hotmail.com>
2021-09-10 08:40:27 -0300

    Exercícios de Java

---
+++ exercicios/java/exercicios05/Exercicio01.java
@@ -0,0 +1,10 @@
+package exercicios05.java08092021;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ exercicios/java/exercicios07/Exercicio01.java
@@ -0,0 +1,10 @@
+package exercicios07.java10092021;
+
+public class Exercicio01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
