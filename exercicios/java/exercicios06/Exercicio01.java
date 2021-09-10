7489a4263668043043cd45c90eead65e91263415
George <george_roots@hotmail.com>
2021-09-10 15:41:27 -0300

    Exercicios de Java

--- exercicios/java/exercicios06/Exercicio01.java
+++ exercicios/java/exercicios06/Exercicio01.java
@@ -1,10 +1,25 @@
 package exercicios06.java09092021;
 
+
+import java.util.Scanner;
+
+/*Faça um programa que crie um vetor por leitura com 5 valores de pontuação de uma
+atividade e o escreva em seguida. Encontre após a maior pontuação e a apresente.*/
+
 public class Exercicio01 {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int numero[] = new int[5];
+		int maior = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		for(int i = 0; i < 5; i++) {
+			numero[i] =  scan.nextInt();
+			System.out.println("Escreva o " + (i+1) + "º número");
+			if(numero[i] > maior)
+				maior = numero[i];
+		}
+		scan.close();
+		System.out.println("Maior número é: " + maior);
+		
 	}
-
 }
